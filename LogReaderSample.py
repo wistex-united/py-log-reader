f6ae455e160dc62c6cdb1731166afa45171c7243
YH_L <yli2565@wisc.edu>
2025-02-13 01:35:37 -0600

    Make the LogReaderSample More comprehensive

--- LogReaderSample.py
+++ LogReaderSample.py
@@ -26,17 +26,14 @@ def checkPointCallback(cnt):
         snapshot = tracemalloc.take_snapshot()
         displayTopMemoryConsumers(snapshot)
 
-@profiler.profile_with_windows
+# @profiler.profile_with_windows
 def main():
     LOG = Log()
-    # LOG.readLogFile("bc18_adam.log")
-    # LOG.readLogFile("Reference.log")
-    LOG.readLogFile("neargoal.log")
+    LOG.readLogFile("sample.log")
     # LOG.eval()
     LOG.eval(isLogFileLarge=True)
 
     # Dump all the representations into json and jpg images
-    # LOG.parseBytes()
     for frame in tqdm.tqdm(LOG.UncompressedChunk.thread("Cognition")):
         # if frame.hasImage:
         #     frame.saveImageWithMetaData()
@@ -82,144 +79,59 @@ def main():
 
     print(f"Continue from {ContinuePos}")
 
-    if not (LOG.outputDir / "data.csv").exists():
-        os.makedirs(LOG.outputDir)
-        with open(LOG.outputDir / "data.csv", "w") as f:
-            print("Create csv")
-            pass
+    # if not (LOG.outputDir / "data.csv").exists():
+    #     os.makedirs(LOG.outputDir)
+    #     with open(LOG.outputDir / "data.csv", "w") as f:
+    #         print("Create csv")
+    #         pass
+    GAME_STATE = LOG.TypeInfoChunk.enumClasses["GameState::State"]
+    MOTION_REQUEST = LOG.TypeInfoChunk.enumClasses["MotionRequest::Motion"]
 
-    with open(LOG.outputDir / "data.csv", "a") as f:
+    with open(LOG.outputDir / "cog_data.csv", "w") as f:
         writer = csv.writer(f)
         if csvLine == 0:
             writer.writerow(
                 [
                     "frameIndex",
-                    "agentLoc",
-                    "ballLoc",
-                    *[
-                        "robotSpeedX",
-                        "robotSpeedY",
-                        "robotSpeedRot",
-                        "kickType",
-                        "kickLength",
-                        "alignPreciselyModified",
-                        "rollOutResult",
-                    ],
+                    "timestamp",
+                    "agentLocX",
+                    "agentLocY",
+                    "agentLocRot",
+                    "motionRequestX",
+                    "motionRequestY",
+                    "motionRequestRot",
                     "jsonFile",
                 ]
             )
-        prevAgentLoc = None
-        prevBallLoc = None
-
-        GAME_STATE = LOG.TypeInfoChunk.enumClasses["GameState::State"]
 
         for frame in LOG.UncompressedChunk.threads["Cognition"]:
-            if frame.absIndex < ContinuePos:
-                continue
-            checkPointCallback(frame.indexCursor)
             print(f"Frame {frame.indexCursor}")
 
             try:
                 gameState = frame["GameState"]
-                playerNumber = gameState["playerNumber"]
-
                 state = gameState["state"]
 
-                if state == GAME_STATE["playing"]:
-                    # The observations we use
-                    agentLoc = frame.agentLoc
-                    ballLoc = frame.ballLoc
-                    teammate_loc = frame.teammateLoc
-                    opponent_loc = frame.opponentLoc
-                    # check if all four properties are not None
-                    assert agentLoc is not None
-                    assert ballLoc is not None
-                    # assert teammate_loc is not None
-                    # assert opponent_loc is not None
+                if state != GAME_STATE["playing"]:
+                    continue
 
-                    obs = OBS.getObservation(agentLoc, ballLoc)
-                    prevAgentLoc = agentLoc if prevAgentLoc is None else prevAgentLoc
-                    prevBallLoc = ballLoc if prevBallLoc is None else prevBallLoc
-                    reward = OBS.getReward(
-                        agentLoc,
-                        prevAgentLoc,
-                        ballLoc,
-                        prevBallLoc,
-                        OBS.ballInGoal(ballLoc),
-                        OBS.ballOutOfFieldBounds(ballLoc),
-                        OBS.ballInGoalArea(ballLoc),
-                        0,
-                    )
-                    act = None
+                motionRequest = frame["MotionRequest"]
+                if motionRequest["motion"] != MOTION_REQUEST["walkAtRelativeSpeed"]:
+                    continue
 
-                    if act is None:
-                        act = [
-                            *frame.motionBasics,
-                            *frame.kickBasics,
-                            # -7,  # stand
-                        ]
-
-                    if act is None:
-                        raise Exception("acts is None")
-                    frame.threadIndex
-                    prev_state = state
-
-                    info = {
-                        "frameIndex": frame.absIndex,
-                        "agentLoc": agentLoc,
-                        "ballLoc": ballLoc,
-                        "robotSpeedX": act[0],
-                        "robotSpeedY": act[1],
-                        "robotSpeedRot": act[2],
-                        "kickType": act[3],
-                        "kickLength": act[4],
-                        "alignPreciselyModified": act[5],
-                        "rollOutResult": frame.rollOutResult,
-                        "jsonFile": frame.jsonName,
-                    }
-                    line = list(info.values())
-                    if frame.absIndex > lastCsvFrame:
-                        writer.writerow(line)
-                    print(line)
-                    # if prev_obs is not None:
-                    #     transitions.append([prev_obs, acts, None, obs, False])
-                    transitions.append([obs, act, info, reward, False])
-                    prevObs = obs
-                    OBS.stepObservationHistory(obs)
-
-                    # obs, acts, infos, next_obs, dones
-                elif state != GAME_STATE["playing"]:
-                    if (
-                        prev_state == GAME_STATE["playing"]
-                        and state == GAME_STATE["ownKickOff"]
-                    ):
-                        if frame.absIndex > lastCsvFrame:
-                            writer.writerow(
-                                [
-                                    frame.index,
-                                    *(["-"] * 8),
-                                    frame.rollOutResult,
-                                    frame.jsonName,
-                                ]
-                            )
-                        last = transitions[-1]
-                        last[4] = True
-
-                        obs, act, info, rewards, dones = map(
-                            np.array, zip(*transitions)
-                        )
-                        np.savez(
-                            LOG.outputDir / f"traj_{frame.absIndex}.npz",
-                            obs=obs,
-                            acts=act,
-                            infos=info,
-                            rewards=rewards,
-                            dones=dones,
-                        )
-                        print(f"Saved {LOG.outputDir / f'traj_{frame.absIndex}.npz'}")
-                else:
-                    raise Exception(f"Unknown state: {state}")
-                prev_state = state
+                agentLoc = frame.agentLoc
+                assert agentLoc is not None
+                info = {
+                    "frameIndex": frame.absIndex,
+                    "timestamp": frame.timestamp,
+                    "agentLocX": agentLoc[0],
+                    "agentLocY": agentLoc[1],
+                    "agentLocRot": agentLoc[2],
+                    "motionRequestX":motionRequest["walkSpeed"]["translation"].x,
+                    "motionRequestY":motionRequest["walkSpeed"]["translation"].y,
+                    "motionRequestRot":motionRequest["walkSpeed"]["rotation"],
+                    "jsonFile": frame.jsonName,
+                }
+                writer.writerow(list(info.values()))
             except (
                 KeyError
             ) as e:  # This is normal, some frame simply doesn't have enough information
@@ -232,6 +144,49 @@ def main():
                 print(f"Exception: {e} at frame {frame.absIndex}")
                 raise
 
+    # with open(LOG.outputDir / "motion_data.csv", "w") as f:
+    #     writer = csv.writer(f)
+    #     if csvLine == 0:
+    #         writer.writerow(
+    #             [
+    #                 "frameIndex",
+    #                 "timestamp",
+    #                 "agentSpeedX",
+    #                 "agentSpeedY",
+    #                 "agentSpeedRot",
+    #                 "jsonFile",
+    #             ]
+    #         )
 
-main()
-print("Done")
+    #     for frame in LOG.UncompressedChunk.threads["Motion"]:
+    #         print(f"Frame {frame.indexCursor}")
+
+    #         try:
+
+    #             motionBasics = frame.motionBasics
+    #             assert motionBasics is not None
+    #             info = {
+    #                 "frameIndex": frame.absIndex,
+    #                 "timestamp": frame.timestamp,
+    #                 "agentSpeedX": motionBasics[0],
+    #                 "agentSpeedY": motionBasics[1],
+    #                 "agentSpeedRot": motionBasics[2],
+    #                 "jsonFile": frame.jsonName,
+    #             }
+    #             writer.writerow(list(info.values()))
+    #         except (
+    #             KeyError
+    #         ) as e:  # This is normal, some frame simply doesn't have enough information
+    #             print(f"KeyError: {e} at frame {frame.absIndex}")
+    #         except AssertionError as e:
+    #             print(f"AssertionError: {e} at frame {frame.absIndex}")
+    #             print(str(frame))
+    #             exit(1)
+    #         except Exception as e:  # Just to add some robusty
+    #             print(f"Exception: {e} at frame {frame.absIndex}")
+    #             raise
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
